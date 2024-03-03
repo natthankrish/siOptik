6c8aaf25a53b67a2b58949c769aa5f90292e7ef8
nathaniacalista01 <nathania.calista01@gmail.com>
2024-03-03 21:46:49 +0700

    feat : take photo function

--- app/src/main/java/com/example/sioptik/HasilFoto.kt
+++ app/src/main/java/com/example/sioptik/HasilFoto.kt
@@ -1,4 +1,17 @@
 package com.example.sioptik
 
-class HasilFoto {
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class HasilFoto :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hasil_foto)
+
+        val imageUri: Uri = Uri.parse(intent.getStringExtra("image_uri"))
+        val imageView: ImageView = findViewById(R.id.image_preview)
+        imageView.setImageURI(imageUri)
+    }
 }
