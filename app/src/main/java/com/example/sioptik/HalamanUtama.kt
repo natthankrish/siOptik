12bf169eb645cd631f9727137e7d2f5dd92634d6
Haziq <haziqabiyyu@gmail.com>
2024-03-03 11:51:34 +0700

    feat: add navigations from halaman utama to other fragments

--- app/src/main/java/com/example/sioptik/HalamanUtama.kt
+++ app/src/main/java/com/example/sioptik/HalamanUtama.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.sioptik.databinding.FragmentHalamanUtamaBinding
 
 class HalamanUtama : Fragment() {
+    private var _binding : FragmentHalamanUtamaBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,7 +20,19 @@ class HalamanUtama : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_halaman_utama, container, false)
+        _binding = FragmentHalamanUtamaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.scanButton.setOnClickListener {
+            val action = HalamanUtamaDirections.actionHalamanUtamaToKamera()
+            val navController = findNavController()
+            navController.navigate(action)
+        }
+
     }
 
 }
