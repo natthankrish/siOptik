7f47aee40aae81c0735649399b814149f49e54e8
Gulilil <juan.csantoso@gmail.com>
2024-03-12 00:03:34 +0700

    feat:loading screen animation

--- app/src/main/java/com/example/sioptik/SplashScreen.kt
+++ app/src/main/java/com/example/sioptik/SplashScreen.kt
@@ -1,15 +1,18 @@
 package com.example.sioptik
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("CustomSplashScreen")
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
+    lateinit var progressBar : ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
@@ -20,11 +23,21 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+
+        progressBar.max = 1500;
+        val curProgress = 1500;
+
+        ObjectAnimator.ofInt(progressBar, "progress", curProgress)
+            .setDuration(1500)
+            .start()
+
         // postDelayed(Runnable, time) method
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 1000) // 3000 = delay/milliseconds.
+        }, 1500) // 3000 = delay/milliseconds.
     }
 }
