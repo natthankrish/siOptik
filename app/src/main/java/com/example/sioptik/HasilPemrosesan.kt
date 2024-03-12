a9265a6d8c999f9869b2879615b6c5582dce9e5c
Nicholas Liem <13521135@std.stei.itb.ac.id>
2024-03-12 13:48:40 +0000

    [IT-34] Penyesuaian halaman Hasil Pemrosessan dengan Flow yang sudah Ditentukan

--- app/src/main/java/com/example/sioptik/HasilPemrosesan.kt
+++ app/src/main/java/com/example/sioptik/HasilPemrosesan.kt
@@ -1,31 +1,44 @@
 package com.example.sioptik
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.example.sioptik.processing_result.FullScreenImageActivity
+import com.example.sioptik.processing_result.fragment_result_april_tag_types.AprilTagType000Fragment
 
 class HasilPemrosesan : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.hasil_pemrosesan)
+        setContentView(R.layout.activity_hasil_pemrosesan)
 
-        val frameLayout = findViewById<FrameLayout>(R.id.imagePlaceholderLeft)
+        val imageView: ImageView = findViewById(R.id.processed_image)
 
-        val imageUriString = intent.getStringExtra("image_uri")
+        val imageUriString = intent.getStringExtra("image_uri") ?: return
         val imageUri = Uri.parse(imageUriString)
 
-        val imageView = ImageView(this).apply {
-            layoutParams = FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT
-            )
-            scaleType = ImageView.ScaleType.CENTER_CROP
-            setImageURI(imageUri)
+        imageView.setImageURI(imageUri)
+
+        imageView.setOnClickListener {
+            val intent = Intent(this, FullScreenImageActivity::class.java)
+            intent.putExtra("imageUri", imageUriString)
+            startActivity(intent)
         }
 
-        frameLayout.addView(imageView)
+        val aprilTagType = intent.getIntExtra("april_tag_type", 0)
+
+        supportFragmentManager.commit {
+            when (aprilTagType) {
+                TYPE_000 -> replace(R.id.fragmentContainerView, AprilTagType000Fragment())
+            }
+        }
+    }
+
+    companion object {
+        const val TYPE_000 = 0
     }
 }
