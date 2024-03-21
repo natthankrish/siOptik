e1139fdf2ef3e7a7f5cbe008c8b491ad410d5fb5
Haziq <haziqabiyyu@gmail.com>
2024-03-21 15:40:18 +0700

    feat: apriltag detection

--- ocr/apriltag_detection.py
+++ ocr/apriltag_detection.py
@@ -2,39 +2,27 @@ from pupil_apriltags import Detector
 import numpy as np
 import cv2
 
+# Note: only runs on linux
 
-# Initialize AprilTag detector
 at_detector = Detector(
-    families="tag16h5",
+    families="tag16h5 tag25h9 tag36h11 tagCircle21h7 tagCircle49h12 tagCustom48h12 tagStandard41h12 tagStandard52h13",
     nthreads=1,
-    quad_decimate=1.0,
-    quad_sigma=0.0,
-    refine_edges=1,
-    decode_sharpening=0.25,
-    debug=0,
+    
 )
 
-# Load the image
-image_path = "test_apriltag.png"
-img = cv2.imread(image_path)  # reads an image in the BGR format
-
-# Convert the image to grayscale
+image_path = "test_apriltag4.jpg"
+img = cv2.imread(image_path)
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect AprilTags in the grayscale image
 tags = at_detector.detect(
     gray_img, estimate_tag_pose=False, camera_params=None, tag_size=None
 )
 
+for detection in tags:
+    corners = detection.corners.astype(np.int32)
+    cv2.rectangle(img, tuple(corners[0]), tuple(corners[2]), (0, 0, 255), 5)
 
-# # Convert the corners to integer values
-corners = tags[0].corners.astype(np.int32)
-
-# # Draw the rectangle
-cv2.rectangle(img, tuple(corners[0]), tuple(corners[2]), (0, 0, 255), 2)
-
-# Save the image with the rectangle
 output_path = "output_image.jpg"
 cv2.imwrite(output_path, img)
-
+print(tags)
 print(f"Image saved as {output_path}")
