7743d5b222b24205c5bb25f1073c2680fb436c56
Haziq <haziqabiyyu@gmail.com>
2024-04-13 16:53:23 +0700

    feat: Riwayat Dao

---
+++ app/src/main/java/com/sioptik/main/riwayat_repository/RiwayatDao.kt
@@ -0,0 +1,16 @@
+package com.sioptik.main.riwayat_repository
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+interface RiwayatDao {
+    @Query("SELECT * FROM riwayat ORDER BY date DESC")
+    fun getAll(userEmail : String): Flow<List<RiwayatEntity>>
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(riwayat: RiwayatEntity)
+    @Delete
+    suspend fun delete(riwayat: RiwayatEntity)
+}
