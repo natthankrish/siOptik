50cac7bd04f26827dda7cee37e6f7da55a08b7bc
Haziq <haziqabiyyu@gmail.com>
2024-04-22 14:04:17 +0700

    feat: RiwayatViewModel

---
+++ app/src/main/java/com/sioptik/main/riwayat_repository/RiwayatViewModel.kt
@@ -0,0 +1,18 @@
+package com.sioptik.main.riwayat_repository
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class RiwayatViewModel(private val riwayatRepository: RiwayatRepository): ViewModel() {
+    fun getAllRiwayat() = riwayatRepository.getAll().asLiveData()
+
+    fun insertRiwayat(riwayat: RiwayatEntity) = viewModelScope.launch {
+        riwayatRepository.insert(riwayat)
+    }
+
+    fun deleteRiwayat(riwayat: RiwayatEntity) = viewModelScope.launch {
+        riwayatRepository.delete(riwayat)
+    }
+}
